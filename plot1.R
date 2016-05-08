ccdb1dcbda14955c5e40505dc35f0cfc0f4f655d
Urvi Gautam <urvigautam45@gmail.com>
2016-05-08 23:33:44 +0545

    changes

---
+++ plot1.R
@@ -0,0 +1,10 @@
+dataFile <- "./exdata/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="blue", main="GLOBAL ACTIVE POWER", xlab="Global Active Power (KILOWATTS)")
+dev.off()
+
